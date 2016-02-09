d7bbfc49a123f6db7989bb0ba1c25acad25af924
Gabriel Salas <ggsalas@Baldomero.local>
2016-02-09 16:23:52 +0100

    martes,  9 de febrero de 2016, 16:23:52 CET

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,32 +2,37 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='ui top borderless inverted blue menu' id='topMenu'>
-        <div className='ui grid three columns centered container'>
-            <div className='column' id='siteTitle'>
-                <a href='#'><h1>Trueke</h1></a>
-            </div>
-            <div className='center aligned column ' id='searchBar'>
-                <div className='ui category search'>
-                    <div className='ui icon input'>
-                        <input className='prompt' placeholder='Buscar anuncios...' type='text' />
-                        <i className='search icon'></i>
-                    </div>
-                    <div className='results'></div>
-                </div>
-            </div>
-            <div className='right aligned column' id='addOneAdd'>
-                <div className='ui button icon red'>
-                    <i className='add icon'></i> <span className='buttonText'>Nuevo Anuncio</span>
-                </div>
-            </div>
-            <div className='left aligned column' id='searchButton'>
-                <div className='ui button icon red'>
-                    <i className='search icon'></i>
-                </div>
-            </div>
-        </div>
-    </div>
+      <div>
+          <section className='ui top borderless inverted blue menu ' id='topMenu'>
+              <div className='ui grid three columns centered container'>
+                  <div className='column' id='siteTitle'>
+                      <a href='#'><h1>Trueke</h1></a>
+                  </div>
+                  <div className='center aligned column ' id='searchBar'>
+                      <div className='ui category search'>
+                          <div className='ui icon input'>
+                              <input className='prompt' placeholder='Buscar anuncios...' type='text' />
+                              <i className='search icon'></i>
+                          </div>
+                          <div className='results'></div>
+                      </div>
+                  </div>
+                  <div className='right aligned column' id='addOneAdd'>
+                      <div className='ui button icon red'>
+                          <i className='add icon'></i> <span className='buttonText'>Nuevo Anuncio</span>
+                      </div>
+                  </div>
+              </div>
+          </section>
+
+          <section className='anuncio anuncio_home'>
+              <p>
+                  Conseguí lo que quieras sin dinero.
+                  <br />
+                  ¡Buscalo ahora! y <u>ofrecé otro a cambio</u>
+              </p>
+          </section>
+      </div>
   )
 }
 
