fcf02dc13df0e8e63d77fb7ab8ddc6e74e4fcc11
carlosvillu <carlosvillu@gmail.com>
2016-02-14 02:28:08 +0100

    domingo, 14 de febrero de 2016, 02:28:08 CET

--- src/components/offert/content.js
+++ src/components/offert/content.js
@@ -1,30 +1,60 @@
 import React from 'react'
 import cx from 'classnames'
 
-const OffertContent = ({text, name, url, author}) => {
-  const offertClass = cx('comment', {
-    author: author
-  })
+export default class OffertContent extends React.Component {
+  static get propTypes () {
+    return {
+      text: React.PropTypes.string,
+      name: React.PropTypes.string,
+      url: React.PropTypes.string,
+      author: React.PropTypes.bool
+    }
+  }
 
-  return (
-    <div className={offertClass}>
-        <span className='author'>{name}</span>
-        <div className='metadata'>
-            <span className='date'>Hoy a las 5:42PM</span>
-        </div>
-        <div className='imageComment'>
-            <img src='http://placehold.it/600x600' alt='lorem' className='ui fluid image' />
-        </div>
-        <div className='text'>{text}</div>
-    </div>
-  )
-}
+  static get defaultProps () {
+    return {
+      text: 'Este anuncio no tiene texto',
+      name: 'Anónimo',
+      url: 'http://placehold.it/200x200',
+      author: false
+    }
+  }
 
-OffertContent.defaultProps = {
-  text: 'Este anuncio no tiene texto',
-  name: 'Anónimo',
-  url: 'http://placehold.it/200x200',
-  author: false
+  constructor (props, context) {
+    super(props, context)
+    this.state = {isExpanded: false}
+  }
+
+
+  render () {
+    const {text, name, url, author} = this.props
+    const offertClass = cx('comment', {
+      author: author
+    })
+    const imgClass = cx('imageComment', {
+      'is-expanded': this.state.isExpanded
+    })
+    return (
+      <div className={offertClass}>
+          <span className='author'>{name}</span>
+          <div className='metadata'>
+              <span className='date'>Hoy a las 5:42PM</span>
+          </div>
+          <div className={imgClass} onClick={this.handleClickImg.bind(this)}>
+              <img src='http://placehold.it/600x600' alt='lorem' className='ui fluid image' />
+          </div>
+          <div className='text'>{text}</div>
+      </div>
+    )
+  }
+  
+  handleClickImg () {
+    this.setState({
+      isExpanded: !this.state.isExpanded
+      
+    })
+  }
+
 }
 
 export default OffertContent
