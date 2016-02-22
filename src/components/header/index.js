718488693f20ba55e57d07938073b0f8acb5cb34
Gabriel Salas <ggsalas@gmail.com>
2016-02-22 20:42:51 +0100

    lunes, 22 de febrero de 2016, 20:42:51 CET

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,26 +1,68 @@
 import React from 'react'
 
-const Header = () => {
-  return (
-      <section className='ui top borderless menu clearfix ' id='topMenu'>
-          <div className='ui grid three columns centered container'>
-              <div className='left aligned column ' id='search'>
-                  <div className='ui button icon blue searchButton' >
-                      <i className='search icon '></i> <span className='buttonText'>Buscar</span>
-                  </div>
-              </div>
-              <div className='center aligned column' id='siteTitle'>
-                  <a href='#'><h1>Trueke</h1></a>
-              </div>
-              <div className='right aligned column' id='addOneAdd'>
-                  <div className='ui button icon blue adButton'>
-                      <i className='add icon'></i> <span className='buttonText'>Nuevo Anuncio</span>
-                  </div>
-              </div>
+class Header extends React.Component {
 
+  constructor () {
+    super()
+    this.state = {query: null}
+  }
+
+  render () {
+      return (
+          <div>
+              <section className='ui top borderless menu clearfix ' id='topMenu'>
+                  <div className='ui grid three columns centered container'>
+                      <div className='left aligned column ' id='search'>
+                          <div className='ui button icon blue searchButton'
+                               onClick={this.handleClickSearch.bind(this)}>
+                              <i className='search icon '></i> <span className='buttonText'>Buscar</span>
+                          </div>
+                      </div>
+                      <div className='center aligned column' id='siteTitle'>
+                          <a href='#'><h1>Trueke</h1></a>
+                      </div>
+                      <div className='right aligned column' id='addOneAdd'>
+                          <div className='ui button icon blue adButton'>
+                              <i className='add icon'></i> <span className='buttonText'>Nuevo Anuncio</span>
+                          </div>
+                      </div>
+
+                  </div>
+              </section>
+
+              <div className='container searchModal' ref='searchModal'>
+                  <span className='buttonClose icon3d searchButton'
+                        onClick={this.handleClickSearch.bind(this)}></span>
+                  <div className='center aligned column searchBar'>
+                      <form className='searchForm' onSubmit={this.handleSubmitSearch.bind(this)}>
+                          <input className='searchInput'
+                                 ref='searchInput'
+                                 type='text'
+                                 name='search'
+                                 placeholder='Escribe aquí'
+                                 value={this.state.query}
+                                 onChange={this.handleChangeSearch.bind(this)}/>
+                      </form>
+                  </div>
+              </div>
           </div>
-      </section>
-  )
+      )
+  }
+
+  handleClickSearch () {
+      $(this.refs.searchModal).toggle(300)
+  }
+
+  handleChangeSearch (evt) {
+    const {value} = evt.target
+    this.setState({query: value});
+  }
+
+  handleSubmitSearch (evt) {
+    evt.preventDefault()
+    this.handleClickSearch()
+    this.props.onChangeSearch(this.refs.searchInput.value)
+  }
 }
 
 export default Header
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -3,20 +3,36 @@ import React from 'react'
 import Header from '../components/header'
 import Ads from '../components/ads'
 
-const Layout = () => {
-  return (
-    <div>
-      <Header />
-      <Ads grid={[
-        {url: 'http://placehold.it/200x200', text: 'text de la primera card', offerts: [{text: 'Oferta Uno', name: 'Nombre', author: true}, {}]},
-        {url: 'http://placehold.it/200x200', text: 'text de la segunda card', offerts: [{text: 'Oferta Uno'}]},
-        {url: 'http://placehold.it/200x200', text: 'text de la tercera card', offerts: [{text: 'Oferta Uno'}]},
-        {url: 'http://placehold.it/200x200', text: 'text de la cuarta card', offerts: [{text: 'Oferta Uno'}]},
-        {url: 'http://placehold.it/200x200', text: 'text de la quinta card', offerts: [{text: 'Oferta Uno'}]},
-        {url: 'http://placehold.it/200x200', text: 'text de la sexta card', offerts: [{text: 'Oferta Uno'}]}
-      ]} />
-    </div>
-  )
+class Layout extends React.Component {
+  constructor () {
+    super()
+    this.state = { grid: [
+      {url: 'http://placehold.it/200x200', text: 'text de la primera card', offerts: [{text: 'Oferta Uno', name: 'Nombre', author: true}, {}]},
+      {url: 'http://placehold.it/200x200', text: 'text de la segunda card', offerts: [{text: 'Oferta Uno'}]},
+      {url: 'http://placehold.it/200x200', text: 'text de la tercera card', offerts: [{text: 'Oferta Uno'}]},
+      {url: 'http://placehold.it/200x200', text: 'text de la cuarta card', offerts: [{text: 'Oferta Uno'}]},
+      {url: 'http://placehold.it/200x200', text: 'text de la quinta card', offerts: [{text: 'Oferta Uno'}]},
+      {url: 'http://placehold.it/200x200', text: 'text de la sexta card', offerts: [{text: 'Oferta Uno'}]}
+    ]}
+  }
+
+  render () {
+    return (
+      <div>
+        <Header onChangeSearch={this.handleChangeSearch.bind(this)}/>
+        <Ads grid={this.state.grid} />
+      </div>
+    )
+  }
+
+  otraFunciondeInstancia () {
+    this.setState()
+  }
+
+  handleChangeSearch (query) {
+    console.log('Layout', query)
+  }
+
 }
 
 export default Layout
